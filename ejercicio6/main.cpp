fd9f98e8a114eec7a44084002c26be1441e32b34
MrShadowDani <dgonzsan@myuax.com>
2023-10-23 22:03:14 +0200

    Guardado Dani
    Ejercicio6 parte2

--- ejercicio6/main.cpp
+++ ejercicio6/main.cpp
@@ -6,5 +6,13 @@ int main() {
     std::cout << "Punteros a Variables: " << std::endl;
     std::cout << "Valor de x: " << x << std::endl;
     std:: cout << "Valor a traves del puntero p: " << *p << std::endl;
+
+    int arr[] = {1, 2, 3, 4, 5};
+    int *ptr =arr;
+    std::cout << "\nPunteros y Arrays: " << std::endl;
+    for (int i = 0; i < 5; i++) {
+        std::cout << "Elemento " << i << ": " << *ptr << std::endl;
+        ptr++;
+    }
     return 0;
 }
