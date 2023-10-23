a86b6adc3144830023fb8a7362fa6b97f1c74ba0
MrShadowDani <dgonzsan@myuax.com>
2023-10-23 21:02:58 +0200

    Guardado Dani
    Ejercicio2

---
+++ ejercicio2/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+int main() {
+    int edad;
+
+    std::cout << "Por favor ingrese su edad: ";
+    std::cin >> edad;
+
+    if (edad >= 18) {
+        std::cout << "Eres mayor de edad.\n";
+    } else {
+        std::cout << "Eres menor de edad.\n";
+    }
+
+    return 0;
+}
