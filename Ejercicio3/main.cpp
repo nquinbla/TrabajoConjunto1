e9d4640b5d8d8683688cbdc663cadf4597ef176f
csantillgar <csantill@myuax.com>
2023-10-25 19:43:27 +0200

    Ejercicio3

---
+++ Ejercicio3/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main() {
+    // Paso 2: Utiliza un bucle for para imprimir los números del 1 al 10
+    for (int i = 1; i <= 10; i++) {
+        // Paso 4: Imprime el índice
+        std::cout << i << "\n";
+    }
+
+    return 0;
+}
